cc2b06e0c7936c1ec49db5d20e9e694eb91ca61b
legit-android-socks <andrei.brumboiu@gmail.com>
2016-05-21 13:26:08 +0300

    Orbit.java Sphero-Orbit project for Techsylvania Hackathon.

---
+++ Orbit.java
@@ -0,0 +1,32 @@
+//: object:Orbit.java
+/**
+ * <h1>Spehro-Orbit</h1>
+ * <h2>Techsylvania Hackathon</h2>
+ * <em>21-22 may 2016</em>
+ * This project aims at creating an application for the Sphero device which simulates
+ * the circular orbit motion in a solar system.
+ * The input parameters are the "masses" of the orbiting objects and the radius of 
+ * the orbit. The application computes the speed of the orbiting body. Color codes
+ * indicate the masses.
+ * Another mode allows the real-time control of mass, radius and velocity in order to
+ * see and understand changes in trajectory with respect to the mentioned parameters.
+ * @author Andrei Brumboiu
+ * @version 1.0 
+ */
+package orbit;
+
+public class Orbit {
+
+	public Orbit() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Sphero-Orbit");
+	}
+
+}
